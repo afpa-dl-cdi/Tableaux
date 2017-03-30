6bb261056dfa7ea2357526eafb6b6bcddb8b6d94
david di Marcantonio <david@code4mac.com>
2017-03-30 09:56:38 +0200

    tableau à plusieurs dimensions

--- Tableaux/Program.cs
+++ Tableaux/Program.cs
@@ -11,11 +11,24 @@ namespace Tableaux
         static void Main(string[] args)
         {
 
-            //déclaration d'un tableau vide de taille de 10 éléments
+            #region tableau à une dimension
             int[] entiers = new int[10];
             string[] journalistes = new string[4] { "maurice", "robert", null, null };
             decimal[] prix = { 12.45m, 45.59m, 14.56m };
             var unTableau = new [] { 1, 3, 4 };
+            #endregion
+
+            #region tableau à plusieurs dimensions
+
+            int[,] matrice = new int[10, 9];
+            //matrice[3, 3] = 5;
+            //int valeur = matrice[5, 5];
+
+            remplirTableauA2Dimension(matrice);
+            affichageTableau2Dimension(matrice);
+
+            int[,,] cube = new int[3, 3, 3];
+            #endregion
 
             #region Copie de tableau à tableau
             int[] cloneEntiers = entiers;            //copie par référence
@@ -49,6 +62,7 @@ namespace Tableaux
 
 
             #region Parcours d'un tableau
+            Console.WriteLine("Tableau à une dimension");
             for (int i = 0; i < entiers.Length ; i++)
             {
                 Console.WriteLine(entiers[i]);
@@ -73,6 +87,40 @@ namespace Tableaux
             for (int i = 0; i < monTableau.Length; i++)
             {
                 monTableau[i] = i;
+            }
+        }
+
+        /// <summary>
+        /// Remplit un tableau à 2 dimensions 
+        /// </summary>
+        /// <param name="deuxD"></param>
+        static void remplirTableauA2Dimension( int [,] deuxD)
+        {
+            for (int i = 0; i < deuxD.GetLength(0); i++)
+            {
+                for (int j = 0; j < deuxD.GetLength(1); j++)
+                {
+                    deuxD[i, j] = j+i;
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Affiche un tableau à deux dimensions 
+        /// </summary>
+        /// <param name="deuxD"></param>
+        static void affichageTableau2Dimension( int [,] deuxD)
+        {
+            Console.WriteLine("Tableau à 2 dimensions");
+            for (int i = 0; i < deuxD.GetLength(1); i++)
+            {
+                for (int j = 0; j < deuxD.GetLength(0); j++)
+                {
+                    Console.Write("{0} ", deuxD[j, i]);
+                }
+
+                Console.WriteLine();
             }
         }
 
